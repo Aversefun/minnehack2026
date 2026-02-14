dcf381a47e67cc7c86f9e534471e756b49200652
FoxMoss <foxmoss@mediaology.com>
2026-02-14 16:43:10 -0600

    working ws

--- laptop-side/src/server.rs
+++ laptop-side/src/server.rs
@@ -2,9 +2,9 @@
 use dioxus::prelude::*;
 use dioxus_fullstack::WebSocketOptions;
 use dioxus_fullstack::Websocket;
-use crate::dioxus_fullstack::JsonEncoding;
+use dioxus_fullstack::JsonEncoding;
 
-#[get("/api/phone_ws/")]
+#[get("/api/phone_ws")]
 pub async fn phone_ws(options: WebSocketOptions) -> Result<Websocket<String, String, JsonEncoding>> {
     Ok(options.on_upgrade(move |mut socket| async move {
         // send back a greeting message
@@ -14,7 +14,7 @@ pub async fn phone_ws(options: WebSocketOptions) -> Result<Websocket<String, Str
 
         // Loop and echo back uppercase messages
         while let Ok(msg) = socket.recv().await {
-            _ = socket.send(msg.to_ascii_uppercase()).await;
+            _ = socket.send("hey!".to_string()).await;
         }
     }))
 }
