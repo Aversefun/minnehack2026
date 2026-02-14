1752cf6bf7f51b76e1b3e066e6321599d42eca07
Vivian Beck <averse.abfun@gmail.com>
2026-02-14 15:18:03 -0600

    aaa

--- laptop-side/src/main.rs
+++ laptop-side/src/main.rs
@@ -1,3 +1,5 @@
+//! laptop code
+
 use dioxus::prelude::*;
 
 #[derive(Debug, Clone, Routable, PartialEq)]
@@ -12,7 +14,6 @@ enum Route {
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
