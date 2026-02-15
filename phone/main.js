e0b6be237671c4f7b12353706b01354d73e72095
FoxMoss <foxmoss@mediaology.com>
2026-02-14 20:44:10 -0600

    gyro update

--- phone/main.js
+++ phone/main.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:8080/api/phone_ws/default_lobby");
+const socket = new WebSocket("ws://localhost:3000/api/phone_ws/test_lobby");
 
 document.getElementById("start-button").addEventListener("click", () => {
   document.getElementById("start-button").remove();
@@ -6,9 +6,8 @@ document.getElementById("start-button").addEventListener("click", () => {
   let gyroscope = new Gyroscope({ frequency: 60 });
 
   gyroscope.addEventListener("reading", (e) => {
-    const encoder = new TextEncoder();
     document.getElementById("debug-info").innerText = `${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}`;
-    socket.send(encoder.encode(JSON.stringify(JSON.stringify({x: gyroscope.x, y: gyroscope.y, z: gyroscope.z}))));
+    socket.send(JSON.stringify({type: "gyro_update", x: gyroscope.x, y: gyroscope.y, z: gyroscope.z}));
   });
   gyroscope.start();
 });
---
+++ server/src/main.rs
@@ -0,0 +1,116 @@
+//! Server-side code.
+
+use std::{collections::HashMap, sync::Arc};
+
+use axum::{
+    Router,
+    extract::{
+        Path,
+        ws::{Message, WebSocket, WebSocketUpgrade},
+    },
+    routing::any,
+};
+use futures_util::{SinkExt, StreamExt, stream::SplitSink};
+use tokio::sync::RwLock;
+
+#[tokio::main]
+async fn main() {
+    let lobbies_real: Arc<
+        RwLock<
+            HashMap<
+                String,
+                (
+                    Option<SplitSink<WebSocket, Message>>,
+                    Option<SplitSink<WebSocket, Message>>,
+                ),
+            >,
+        >,
+    > = Arc::new(RwLock::new(HashMap::new()));
+
+    let lobbies = lobbies_real.clone();
+
+    let app = Router::new()
+        .route(
+            "/api/phone_ws/{lobby}",
+            any(
+                move |ws: WebSocketUpgrade, Path(lobby): Path<String>| async move {
+                    ws.on_upgrade(async move |socket| {
+                        let (sender, mut receiver) = socket.split();
+
+                        match lobbies.write().await.entry(lobby.clone()) {
+                            std::collections::hash_map::Entry::Occupied(mut entry) => {
+                                entry.get_mut().0 = Some(sender)
+                            }
+                            std::collections::hash_map::Entry::Vacant(entry) => {
+                                _ = entry.insert((Some(sender), None))
+                            }
+                        }
+                        tokio::spawn(async move {
+                            let lobby = lobby;
+                            while let Some(msg) = receiver.next().await {
+                                if let Ok(msg) = msg {
+                                    print!("{}\n", msg.to_text().unwrap());
+                                    let mut lobbies = lobbies.write().await;
+                                    let sink = lobbies.get_mut(&lobby).unwrap().1.as_mut();
+                                    if let Some(sink) = sink {
+                                        sink.send(msg).await.unwrap();
+                                    }
+                                } else {
+                                    break;
+                                };
+                            }
+                            let mut lobbies = lobbies.write().await;
+                            if lobbies.get(&lobby).unwrap().1.is_none() {
+                                lobbies.remove(&lobby);
+                            } else {
+                                lobbies.get_mut(&lobby).unwrap().0.take();
+                            }
+                        });
+                    })
+                },
+            ),
+        )
+        .route(
+            "/api/laptop_ws/{lobby}",
+            any(
+                move |ws: WebSocketUpgrade, Path(lobby): Path<String>| async move {
+                    let lobbies = lobbies_real;
+                    ws.on_upgrade(async move |socket| {
+                        let (sender, mut receiver) = socket.split();
+
+                        match lobbies.write().await.entry(lobby.clone()) {
+                            std::collections::hash_map::Entry::Occupied(mut entry) => {
+                                entry.get_mut().1 = Some(sender)
+                            }
+                            std::collections::hash_map::Entry::Vacant(entry) => {
+                                _ = entry.insert((None, Some(sender)))
+                            }
+                        }
+                        tokio::spawn(async move {
+                            let lobby = lobby;
+                            while let Some(msg) = receiver.next().await {
+                                if let Ok(msg) = msg {
+                                    let mut lobbies = lobbies.write().await;
+                                    let sink = lobbies.get_mut(&lobby).unwrap().0.as_mut();
+                                    if let Some(sink) = sink {
+                                        sink.send(msg).await.unwrap();
+                                    }
+                                } else {
+                                    break;
+                                };
+                            }
+                            let mut lobbies = lobbies.write().await;
+                            if lobbies.get(&lobby).unwrap().0.is_none() {
+                                lobbies.remove(&lobby);
+                            } else {
+                                lobbies.get_mut(&lobby).unwrap().1.take();
+                            }
+                        });
+                    })
+                },
+            ),
+        );
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
