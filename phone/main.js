59dc0fedead78a9c4cebeceb820cdfcd3205f40f
Vivian Beck <averse.abfun@gmail.com>
2026-02-14 20:02:01 -0600

    merge phone stuff in

---
+++ phone/main.js
@@ -0,0 +1,14 @@
+const socket = new WebSocket("ws://localhost:8080/api/phone_ws/test_lobby");
+
+document.getElementById("start-button").addEventListener("click", () => {
+  document.getElementById("start-button").remove();
+
+  let gyroscope = new Gyroscope({ frequency: 60 });
+
+  gyroscope.addEventListener("reading", (e) => {
+    const encoder = new TextEncoder();
+    document.getElementById("debug-info").innerText = `${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}`;
+    socket.send(encoder.encode(JSON.stringify(JSON.stringify({x: gyroscope.x, y: gyroscope.y, z: gyroscope.z}))));
+  });
+  gyroscope.start();
+});
