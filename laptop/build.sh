bbb3305cd20fc04714a619adb14964e9e01f5d38
Vivian Beck <averse.abfun@gmail.com>
2026-02-14 21:08:09 -0600

    oops

--- laptop/build.sh
+++ laptop/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cargo build --target=wasm32-unknown-unknown --debug
+cargo build --target=wasm32-unknown-unknown
 wasm-bindgen --out-dir ./out/ --target web target/wasm32-unknown-unknown/debug/laptop.wasm 
