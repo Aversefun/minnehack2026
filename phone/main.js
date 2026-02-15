75be3540fe4d82a7313a2816b7756122623274da
FoxMoss <foxmoss@mediaology.com>
2026-02-14 21:02:47 -0600

    add screen buttons

--- phone/main.js
+++ phone/main.js
@@ -2,6 +2,7 @@ const socket = new WebSocket("ws://localhost:3000/api/phone_ws/test_lobby");
 
 document.getElementById("start-button").addEventListener("click", () => {
   document.getElementById("start-button").remove();
+    document.getElementById("game-display").style.display = "grid";
 
   let gyroscope = new Gyroscope({ frequency: 60 });
 
@@ -10,4 +11,9 @@ document.getElementById("start-button").addEventListener("click", () => {
     socket.send(JSON.stringify({type: "gyro_update", x: gyroscope.x, y: gyroscope.y, z: gyroscope.z}));
   });
   gyroscope.start();
+
+
+  // for(let button in ["a", "b", "x", "y"]){
+  //   document.getElementById(`button-`).addEventListener("click", () => {});
+  // }
 });
